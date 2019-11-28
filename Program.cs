d1eef888889090161df6a95be2d2416a53957f66
ahmed <ahmedseyam1993@gmail.com>
2019-11-28 09:36:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            Console.WriteLine("enter your input");
+            string input = Console.ReadLine();
+            string[] arr = input.Split(' ');
+            foreach (string token in arr)
+            {
+                int val;
+                if (int.TryParse(token,out val))
+                {
+                    s.push(val);
+
+                }
+                else
+                {
+                    int rhs = s.pop();
+                    int lhs = s.pop();
+                    switch (token)
+                    {
+                        case "+":
+                            s.push(lhs + rhs);
+                            break;
+                        case "-":
+                           s.push(Math.Abs(lhs - rhs));
+                            break;
+                        case "*":
+                            s.push(Math.Abs(lhs * rhs));
+                            break;
+                        case "/":
+                            s.push(Math.Abs(lhs / rhs));
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+
+
+            }
+            Console.WriteLine("the result is\t" + s.pop());
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Stack
+    {
+      static  int capacity = 1000;
+        int top;
+        int[] stack = new int [capacity];
+        public Stack()
+        {
+            top = -1;
+        }
+        public bool push (int data)
+        {
+            if (top==capacity-1)
+            {
+                Console.WriteLine("Stack overflow");
+                return false;
+
+            }
+            else
+            {
+                stack[++top]=data;
+                return true;
+            }
+
+
+        }
+        public int pop()
+        {
+            if (top<0)
+            {
+                Console.WriteLine("stack underflow");
+                return 0;
+
+            }
+            else
+            {
+                int value = stack[top--];
+                return value;
+            }
+        }
+        public void PrintStack()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Items in the Stack are :");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+
+    }
+}
